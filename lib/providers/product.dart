f6e98e1e1412bb602c70f6d34d6f80bdb281b8a4
ScottFarestrand <ScottFarestrand@gmail.com>
2020-12-28 09:10:07 -0800

    Challenge # 1. Handle Favorite Toggle to FB

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/foundation.dart';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import '../models/http_exception.dart';
+
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,9 +22,23 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavoriteStatus() {
+  void toggleFavoriteStatus()  async {
+    // final id = this.id
+    print(id);
+    final url = 'https://shopapp-ab5a0-default-rtdb.firebaseio.com/products/$id.json';
     isFavorite = !isFavorite;
     notifyListeners();
+    final response =  await http.patch(url,
+        body: json.encode({
+          'isFavorite': isFavorite,
+        }));
+    print(response.statusCode);
+    if (response.statusCode >= 400) {
+      print('throwing');
+      isFavorite = !isFavorite;
+      notifyListeners();
+      throw HttpException('Could not toggle Favorite');
+    }
   }
 }
 
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -16,6 +16,7 @@ class ProductItem extends StatelessWidget {
   Widget build(BuildContext context) {
     final product = Provider.of<Product>(context, listen: false);
     final cart = Provider.of<Cart>(context, listen: false);
+    final scaffoldMessenger = ScaffoldMessenger.of(context);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -39,8 +40,18 @@ class ProductItem extends StatelessWidget {
                 product.isFavorite ? Icons.favorite : Icons.favorite_border,
               ),
               color: Theme.of(context).accentColor,
-              onPressed: () {
-                product.toggleFavoriteStatus();
+              onPressed: () async {
+                try {
+                  await product.toggleFavoriteStatus();
+                } catch (error){
+                  print('error');
+                 scaffoldMessenger.showSnackBar( SnackBar(
+                    content: Text(
+                      'Toggling Favorite failed!',
+                      textAlign: TextAlign.center,
+                    ),
+                  ));
+                }
               },
             ),
           ),
